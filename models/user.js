09423d5f2d1356d9e0b16ccece123e708ff720ed
jeeva m <jeelion22@gmail.com>
2024-05-11 18:58:46 +0530

    added models for user and admin

---
+++ models/user.js
@@ -0,0 +1,117 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const validator = require("validator");
+
+const userSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    minlength: [3, "First name should be at least 3 characters."],
+    maxlength: [12, "First name should not exceed 12 characters."],
+  },
+  lastname: {
+    type: String,
+    required: true,
+    minlength: [1, "Second name should be at least 1 character."],
+    maxlength: [15, "Second name should not exceed 15 characters."],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (phone) =>
+        validator.isMobilePhone(phone, "any", { strictMode: false }),
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+  prevPhones: [],
+  isEmailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isPhoneVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  passwordHash: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return this.isEmailVerified ? v : true;
+      },
+      message: "Password is required",
+    },
+  },
+
+  userCategory: {
+    type: String,
+    enum: ["communityUploader", "reuniteSeeker", "both"],
+    default: "communityUploader",
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  accountRegisteredAt: { type: Date, default: Date.now },
+  accountDeletetedAt: {
+    type: Date,
+  },
+
+  whoDeleted: [
+    {
+      userId: {
+        _id: false,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+
+      role: {
+        type: String,
+        enum: ["Admin", "User"],
+      },
+
+      deletionDate: { type: Date, default: Date.now },
+    },
+  ],
+
+  emailVerificationToken: String,
+  emailVerificationTokenExpires: Date,
+  otp: String,
+  otpExpires: Date,
+});
+
+userSchema.methods.createEmailVerificationToken = function () {
+  const emailToken = crypto.randomBytes(32).toString("hex");
+  this.emailVerificationToken = crypto
+    .createHash("sha256")
+    .update(emailToken)
+    .digest("hex");
+  this.emailVerificationTokenExpires = new Date(Date.now() + 30 * 60 * 1000);
+
+  return emailToken;
+};
+
+userSchema.methods.createOTPHash = function (otp) {
+  this.otp = crypto.createHash("sha256").update(otp).digest("hex");
+  this.otpExpires = new Date(Date.now() + 10 * 60 * 1000);
+};
+
+module.exports = mongoose.model("User", userSchema, "users");
