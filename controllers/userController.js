7394b6242b96613a4bedd776271eed5219095fc6
jeeva m <jeelion22@gmail.com>
2024-07-04 21:23:53 +0530

    added obj to clearCookie method

--- controllers/userController.js
+++ controllers/userController.js
@@ -414,7 +414,7 @@ const userController = {
 
   logout: async (req, res) => {
     try {
-      res.clearCookie("token");
+      res.clearCookie("token", { path: "/", secure: true, sameSite: "Strict" });
 
       res.status(200).json({ message: "logout successful!" });
     } catch (error) {
