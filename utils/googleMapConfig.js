53ad5b0e23d75681285c2060eaacbd63c5261828
jeeva m <jeelion22@gmail.com>
2024-07-08 15:19:31 +0530

    removed unused package dependent files

--- utils/googleMapConfig.js
+++
@@ -1,9 +0,0 @@
-import { Loader } from "@googlemaps/js-api-loader";
-import { GOOGLE_MAP_API_KEY, GOOGLE_MAP_VERSION } from "./config";
-
-const loader = new Loader({
-  apiKey: GOOGLE_MAP_API_KEY,
-  version: GOOGLE_MAP_VERSION,
-});
-
-module.exports = loader;
--- utils/twilioClient.js
+++
@@ -1,8 +0,0 @@
-const config = require("./config");
-
-const accountSid = config.TWILIO_ACCOUNTSID;
-const authToken = config.TWILIO_AUTH_TOKEN;
-
-const client = require("twilio")(accountSid, authToken);
-
-module.exports = client;
