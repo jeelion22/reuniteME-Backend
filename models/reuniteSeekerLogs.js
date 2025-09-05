547794d4ff6e1fab7cbc81b4419e7f55bc0be45a
jeeva <jeelion22@gmail.com>
2025-09-05 11:21:22 +0530

    timestamps field added to the schema

--- models/reuniteSeekerLogs.js
+++ models/reuniteSeekerLogs.js
@@ -1,59 +1,63 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const reuniteSeekersLogsSchema = new mongoose.Schema({
-  contributionId: {
-    type: String,
-    required: true,
-  },
-  visitorsId: {
-    type: String,
-    required: true,
-  },
-  relationship: {
-    type: String,
-    required: true,
-  },
+const reuniteSeekersLogsSchema = new mongoose.Schema(
+  {
+    contributionId: {
+      type: String,
+      required: true,
+    },
+    visitorsId: {
+      type: String,
+      required: true,
+    },
 
-  lastSeen: {
-    type: Date,
-    required: true,
-  },
-  purpose: {
-    type: String,
-    required: true,
-  },
-  contactNo: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (phone) =>
-        validator.isMobilePhone(phone, "any", { strictMode: false }),
-      message: (props) => `${props.value} is not a valid phone number!`,
+    relationship: {
+      type: String,
+      required: true,
+    },
+
+    lastSeen: {
+      type: Date,
+      required: true,
+    },
+    purpose: {
+      type: String,
+      required: true,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (phone) =>
+          validator.isMobilePhone(phone, "any", { strictMode: false }),
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    },
+    meetingDate: {
+      type: Date,
+      required: true,
+    },
+    willUpdate: {
+      type: Boolean,
+      required: true,
+    },
+    visitedDate: {
+      type: Date,
+      default: Date.now,
+    },
+    checking: {
+      type: Boolean,
+      default: true,
+    },
+    status: {
+      type: String,
+      enum: ["rescued", "not-rescued"],
+      default: "not-rescued",
     },
   },
-  meetingDate: {
-    type: Date,
-    required: true,
-  },
-  willUpdate: {
-    type: Boolean,
-    required: true,
-  },
-  visitedDate: {
-    type: Date,
-    default: Date.now,
-  },
-  checking: {
-    type: Boolean,
-    default: true,
-  },
-  status: {
-    type: String,
-    enum: ["rescued", "not-rescued"],
-    default: "not-rescued",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model(
   "ReuniteSeekersLogs",
