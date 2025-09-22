38d4ee77c8461256599864ba138fbcb1dcbf8cc9
jeeva <jeelion22@gmail.com>
2025-09-22 14:28:21 +0530

    bug fix in response message

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,7 +82,7 @@ const userController = {
 
       res.status(201).json({
         status: "success",
-        message: `${newUser && "User created successfully."}`,
+        message: newUser && "User created successfully.",
       });
     } catch (error) {
       console.log(error);
