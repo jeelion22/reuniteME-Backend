a6c100a36379e2167af290ad08b6599d5781f6b6
jeeva <jeelion22@gmail.com>
2025-10-02 15:44:26 +0530

    required secretes updated

--- utils/config.js
+++ utils/config.js
@@ -30,6 +30,10 @@ const ALGORITHM = process.env.ALGORITHM;
 
 const IV_LENGTH = process.env.IV_LENGTH;
 
+const CLIENT_ID = process.env.client_id;
+const CLIENT_SECRET = process.env.client_secret;
+const REDIRECT_URL = process.env.redirect_url;
+
 // const LOCAL_MASTER_KEY = process.env.LOCAL_MASTER_KEY;
 // const DB_NAME = process.env.DB_NAME;
 
@@ -64,4 +68,8 @@ module.exports = {
   ENC_KEY,
   ALGORITHM,
   IV_LENGTH,
+
+  CLIENT_SECRET,
+  CLIENT_ID,
+  REDIRECT_URL,
 };
