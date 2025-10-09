544ba9e031b580293b74e09dc26074fe9a5e87cb
jeeva <jeelion22@gmail.com>
2025-10-10 01:20:31 +0530

    remove empty line

--- models/blockedToken.js
+++ models/blockedToken.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const blockedTokenSchema = new mongoose.Schema({
-    token: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    expiresAt: {
-type: Date, 
-required: true
-    }
-})
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+});
 
 // automatically remove token after they expire
-blockedTokenSchema.index({expiresAt: 1}, {expireAfterSeconds: 0})
+blockedTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
-module.exports = mongoose.model("BlockedToken", blockedTokenSchema)
+module.exports = mongoose.model("BlockedToken", blockedTokenSchema);
