9466a18cf697317f3497138671a697ed6d0e3c60
jeeva <jeelion22@gmail.com>
2025-10-10 01:21:31 +0530

    deleted routes not need further

--- routes/oAuth2Routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { googleAuthCallBack } = require("../controllers/oAuth2Controller");
-
-const oAuth2Router = express.Router();
-
-oAuth2Router.get("/auth/google/callback", googleAuthCallBack);
-
-module.exports = oAuth2Router;
