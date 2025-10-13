92772115d6630d5fcaf6eff449b0991ccc791855
jeeva <jeelion22@gmail.com>
2025-10-13 11:09:37 +0530

    add sendgrid for email verification

---
+++ utils/sendGridEmail.js
@@ -0,0 +1,26 @@
+const sgMail = require("@sendgrid/mail");
+const { SENDGRID_API_KEY, SENDER_EMAIL } = require("./config");
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const sendEmailToVerifyEmail = async (option) => {
+  const msg = {
+    to: option.email,
+    from: `ReUniteME <${SENDER_EMAIL}>`,
+    subject: option.subject,
+    html: option.message,
+  };
+
+  try {
+    const result = await sgMail.send(msg);
+    console.log("✅ Email sent successfully!");
+  } catch (error) {
+    console.error(
+      "❌ Error sending email:",
+      error.response?.body || error.message
+    );
+    throw error;
+  }
+};
+
+module.exports = sendEmailToVerifyEmail;
